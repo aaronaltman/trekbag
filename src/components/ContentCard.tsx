64037c2796bdb00d9b0ae0ebd08ca41a0e52456d
aka <aaronkaltman@gmail.com>
2023-12-29 18:11:44 -0500

    not quite

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -5,33 +5,35 @@ import Header from "./Header";
 import PackingList from "./PackingList";
 import Sidebar from "./Sidebar";
 
-let items = "";
-
 export default function ContentCard() {
   const [theItems, setTheItems] = useState<string[]>([]);
+  const [currentItem, setCurrentItem] = useState(""); // New state for input
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault(); // Prevent default form submit action
-    setTheItems((prevItems) => [...prevItems, items]); // Add new item to the list
+    e.preventDefault();
+    if (currentItem.trim() !== "") {
+      setTheItems((prevItems) => [...prevItems, currentItem]);
+      setCurrentItem(""); // Clear the input field after submission
+    }
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    items = e.target.value;
+    setCurrentItem(e.target.value);
   };
 
-  // return
   return (
-    <section className="flex flex-col bg-slate-50 w-[50rem]">
+    <section className="flex flex-col bg-slate-50 w-full md:w-2/3 mx-auto shadow-lg rounded-lg overflow-hidden">
       <Header />
-      <div className="flex justify-between px-10 py-10">
-        <div className="flex-1">
+      <div className="flex flex-col md:flex-row justify-between p-10">
+        <div className="flex-1 md:mr-4">
           <PackingList theItems={theItems} />
         </div>
-        <div className="w-64">
+        <div className="flex-1 md:ml-4 mt-4 md:mt-0">
           <Sidebar
             theItems={theItems}
             handleSubmit={handleSubmit}
-            items={items}
+            currentItem={currentItem} // pass the currentItem as prop
+            handleInputChange={handleInputChange}
           />
         </div>
       </div>
